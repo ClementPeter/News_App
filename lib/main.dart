6e7cba181c15fad152a6e385dd2d3f1b158c4997
ClementPeter <61213263+ClementPeter@users.noreply.github.com>
2022-12-31 23:23:08 +0100

    setup : Random API fetching

--- lib/main.dart
+++ lib/main.dart
@@ -44,7 +44,7 @@ class _MyHomePageState extends State<MyHomePage> {
         itemCount: 10,
         itemBuilder: (context, index) {
           return Container(
-              //color: Colors.red,
+              
               child: NewsContainer(
             title:
                 'Apple is planning to launch the largest MacBook Air ever at its spring 2023 event',
@@ -52,8 +52,8 @@ class _MyHomePageState extends State<MyHomePage> {
                 'We’ve been hearing rumors about a new, larger MacBook Air for a while now, but according to a new report from an accurate source, the laptop might make finally make its debut in the next few months. which means the new laptop could be revealed in the spring. Apple is reportedly planning several Mac announcements at its annual spring event, including refreshes to the MacBook Pro and Mac mini, and possibly a revival of the iMac Pro". ',
             urlToImage:
                 'https://images.unsplash.com/photo-1530893609608-32a9af3aa95c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8bWFjYm9vayUyMHByb3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60',
-            //urlToImage: 'https://picsum.photos/250?image=9',
-            // url: 'https://picsum.photos/250?image=9',
+            
+            
             url:
                 'https://www.gizmochina.com/2022/12/18/oukitel-rt3-mini-rugged-tablet-price-specs-launch-date/',
           ));
